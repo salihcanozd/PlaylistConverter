854b3c5f610eb42171ee49c6215496495088c58c
SalihcanOzdemir <salihcanozdemir12@gmail.com>
2023-06-01 20:35:29 +0300

    Yeniden düzenlemeler ve bitiş

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Youtube şarkı playlistini Spotify şarkı playlistine dönüştürür.
+
+main.py'de bulunan Youtube API anahtarı ve Spotify client_id      bilgilerini girdikten sonra çalıştırabilirsiniz
+                                                   client_secret
+                                                   redirect_uri
--- main.py
+++ main.py
@@ -1,51 +1,57 @@
-import os
-import googleapiclient.discovery
-from googleapiclient.errors import HttpError
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from googleapiclient.discovery import build
 
-# Set up the API client
-api_service_name = "youtube"
-api_version = "v3"
-api_key = ""  # Replace with your own YouTube Data API key
+#YouTube API key 
+youtube_api_key = '#'  
 
-# Function to retrieve video names from a playlist
-def get_video_names(playlist_id):
-    try:
-        # Create a YouTube Data API client
-        youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+# Spotify API credentials
+client_id = '#'
+client_secret = '#'
+redirect_uri = '#'
 
-        # Retrieve the playlist items
-        playlist_items = youtube.playlistItems().list(
-            part="snippet",
-            playlistId=playlist_id,
-            maxResults=150  # Adjust the maximum number of results as needed
-        ).execute()
+# YouTube playlist ID
+youtube_playlist_id = input("Playlist ID: ")  # Replace with your YouTube playlist ID
 
-        # Extract video names from the playlist items
-        video_names = [item["snippet"]["title"] for item in playlist_items["items"]]
+# Create a YouTube client object
+youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-        return video_names
+# Create a Spotify client object
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri=redirect_uri,
+                                               scope='playlist-modify-private'))
 
-    except HttpError as e:
-        print("An HTTP error occurred:")
-        print(e)
-        return None
+# Get YouTube playlist items
+playlist_items = []
+next_page_token = None
+while True:
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=youtube_playlist_id,
+        maxResults=50,
+        pageToken=next_page_token
+    )
 
-# Main function
-def main():
-    # Prompt the user to enter the YouTube playlist ID
-    playlist_id = input("Enter the YouTube playlist ID: ")
+    response = request.execute()
+    playlist_items.extend(response['items'])
+    next_page_token = response.get('nextPageToken')
+    if not next_page_token:
+        break
 
-    # Retrieve video names from the playlist
-    video_names = get_video_names(playlist_id)
-    counter = 0
-    # Display the video names
-    if video_names:
-        print("\nVideo Names:")
-        for name in video_names:
-            counter = counter + 1
-            print(str(counter)+ ". " + name)
-    else:
-        print("Failed to retrieve video names.")
+# Extract video IDs from YouTube playlist items
+video_ids = [item['snippet']['resourceId']['videoId'] for item in playlist_items]
 
-if __name__ == "__main__":
-    main()
+# Search for each video on Spotify and add it to the playlist
+playlist_name = input("New Playlist Name: ")
+user_id = sp.me()['id']  # Retrieve the current user's ID
+playlist = sp.user_playlist_create(user_id, playlist_name, public=False)
+
+for item in playlist_items:
+    title = item['snippet']['title']
+    search_results = sp.search(q=title, type='track', limit=1)
+    if search_results['tracks']['items']:
+        track_uri = search_results['tracks']['items'][0]['uri']
+        sp.user_playlist_add_tracks(user_id, playlist['id'], [track_uri])
+
+print("Playlist conversion completed!")
